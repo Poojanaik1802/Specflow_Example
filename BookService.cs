3aefba82dde0fbe9f55f4388399efd8fedb80b46
NEW <NEW@DESKTOP-127GECA>
2024-08-05 22:09:37 +0530

    Add project files.

---
+++ BookService.cs
@@ -0,0 +1,16 @@
+﻿namespace Specflow_Example
+{
+    public class BookService
+    {
+        private readonly List<Book> _bookList= new List<Book>();
+        public void AddBook(Book book)
+        {
+            _bookList.Add(book);
+        }
+
+        public Book GetBook(string bookName)
+        {
+            return _bookList.Single(x=>x.Name == bookName);
+        }
+    }
+}
